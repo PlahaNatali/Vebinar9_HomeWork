c5f1487f983d822cabf17f3db38ef46092eeaae1
Nataliya <krasiukovan@mail.ru>
2023-02-12 13:11:10 +0300

    Add task about sum numbers

---
+++ Task002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine(SumNumbers(1, 15));
+
+int SumNumbers(int m, int n)
+{
+    if (m == n)
+    {
+        return n;
+    }
+    else
+    {
+        n += SumNumbers(m, n - 1);
+    }
+    return n;
+}
